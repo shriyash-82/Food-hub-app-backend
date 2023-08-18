878f7b466e146d61db7509c9cac3b93cf0e92a71
shriyash awasthi <shriyashawasthi01feb@gmail.com>
2023-08-19 00:16:27 +0530

    changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const createUser = require("./routes/createUser")
 const userLogin = require("./routes/logIn")
 const displayData = require("./routes/displayData")
 const orderData = require("./routes/Orderdata")
+const PORT = process.env.port || 5000
 // implementing cors
  app.use( (req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -21,6 +22,6 @@ app.use("/api",createUser )
 app.use("/api", userLogin)
 app.use("/api", displayData)
 app.use("/api", orderData)
-app.listen(5000, (req,res) => {
+app.listen(PORT, (req,res) => {
     console.log("server started")
 })
